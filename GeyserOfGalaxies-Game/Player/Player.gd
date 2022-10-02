ce5fa30ed8a72405afb0d6774071903ec8ddfd13
Rainmaker0102 <35494197+Rainmaker0102@users.noreply.github.com>
2022-10-01 20:04:13 -0400

    yee

--- GeyserOfGalaxies-Game/Player/Player.gd
+++ GeyserOfGalaxies-Game/Player/Player.gd
@@ -5,13 +5,14 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 export (float) var rotation_speed = 1.5
+var can_shoot = true
 
 var velocity = Vector2()
 var rotation_dir = 0
 
 func get_input():
 	rotation_dir = 0
-	velocity = Vector2()
+	velocity = Vector2(0,0)
 	if Input.is_action_pressed("ui_right"):
 		rotation_dir += 1
 	if Input.is_action_pressed("ui_left"):
@@ -20,6 +21,7 @@ func get_input():
 		velocity = Vector2(0, -speed).rotated(rotation)
 	if Input.is_action_pressed("ui_down"):
 		velocity = Vector2(0, speed).rotated(rotation)
+		
 
 func _physics_process(delta):
 	get_input()
